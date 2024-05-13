8a95b6552b5f6bc32e50470ce5fdadc697e10170
MACKYSAN233 <153158215+MACKYSAN233@users.noreply.github.com>
2024-05-13 14:04:32 +0800

    Add files via upload

---
+++ Ebike/Files/Files/E-Bike.php
@@ -0,0 +1,325 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="swiper/css/swiper.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="icon" type= "image/x-icon" href="C:\xampp\htdocs\EBIKE\Ebike\Files\Files\img\Logo (2).jpg"
+    </head>
+    <body>
+       <header class="header">
+           <a href="#" class="logo"><span>E</span>Zone</a>
+           <nav class="navbar">
+               <a href="Index.html">home</a>
+               <a href="BAHAHAHAHA.html">E-bike</a>
+               <a href="#services">services</a>
+               <a href="#contact us">contact us</a>
+               <a href="Cart.html"><i class="fa fa-shopping-cart" style="font-size:36px"></i></a>
+           </nav>
+       </header>
+       <section class="home" id="home">
+           <img src="C:\Users\marka\Downloads\50.png" alt="">
+           <a href="BAHAHAHAHA.html" class="btn">Shop Now</a>
+       </section>
+       <!--End Of Home-->
+       <section class="motorCycle" id="motor">
+           <h1 class="heading">Ebike<span>motorCycle</span></h1>
+           <div class="swiper-container motor-slider">
+               <div class="swiper-wrapper wrapper">
+                   <div class="swiper-slide box">
+                       <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/WSPWHT.jpg" alt="">
+                       <div class="content">
+                           <h3>E-bike WSP</h3>
+                           <div class="price"><span>price : </span> &#8369;43,800</div>
+                           <p>
+                               new
+                               <span class="fa fa-circle"></span>2021
+                               <span class="fa fa-circle"></span>automatic
+                               <span class="fa fa-circle"></span>Lead-Acid,Lithium Iron
+                               <span class="fa fa-circle"></span>35-45 kph
+                           </p>
+                       </div>
+                   </div>
+                   <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/EMCMOONSILVER.jpg" alt="">
+                    <div class="content">
+                        <h3>E-Bike EMC-GOLF</h3>
+                        <div class="price"><span>price : </span>&#8369;88,000</div>
+                        <p>
+                            new
+                            <span class="fa fa-circle"></span>2021
+                            <span class="fa fa-circle"></span>automatic
+                            <span class="fa fa-circle"></span>Lead Acid
+                            <span class="fa fa-circle"></span>25-35 Kph
+                        </p>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERV2WHT.jpg" alt="">
+                    <div class="content">
+                        <h3>E-Bike ERV2</h3>
+                        <div class="price"><span>price : </span> &#8369;42,800</div>
+                        <p>
+                            new
+                            <span class="fa fa-circle"></span>2021
+                            <span class="fa fa-circle"></span>automatic
+                            <span class="fa fa-circle"></span>Lead Acid
+                            <span class="fa fa-circle"></span>24-30 Kph
+                        </p>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERVS2GREY2_i6ou.jpg" alt="">
+                    <div class="content">
+                        <h3>E-Bike ERVS2</h3>
+                        <div class="price"><span>price : </span>&#8369;59,800</div>
+                        <p>
+                            new
+                            <span class="fa fa-circle"></span>2021
+                            <span class="fa fa-circle"></span>automatic
+                            <span class="fa fa-circle"></span>Lead Acid, Lithium Iron
+                            <span class="fa fa-circle"></span>25-32 Kph
+                        </p>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/UNITSWEBSITESIZETEMPLATE.jpg" alt="">
+                    <div class="content">
+                        <h3>E-Bike ERV</h3>
+                        <div class="price"><span>price : </span>&#8369;36,000</div>
+                        <p>
+                            new
+                            <span class="fa fa-circle"></span>2021
+                            <span class="fa fa-circle"></span>automatic
+                            <span class="fa fa-circle"></span>Lead Acid, Lithium Iron
+                            <span class="fa fa-circle"></span>24-35 Kph
+                        </p>
+                    </div>
+                </div>
+               </div>
+               <div class="swiper-pagination"></div>
+           </div>
+       </section>
+       <!--End Of motorCycle-->
+       <section class="services" id="services">
+           <h1 class="heading">Our <span>E-Bike Services</span></h1>
+           <div class="box-container">
+               <div class="box">
+                   <i class="fa fa-cog"></i>
+                   <h3>E-Bike Maintenance</h3>
+                   <p>
+                    E-bike maintenance is the upkeep of electric bicycles to ensure they perform well, stay safe, and last longer. This includes tasks like checking the battery, inspecting electrical parts, tuning gears and brakes, and addressing wear and tear.
+                   </p>
+                   <a href="#" class="btn">read more</a>
+               </div>
+               <div class="box">
+                <i class="fa fa-motorcycle"></i>
+                <h3>Selling E-Bike</h3>
+                <p>
+                    Selling e-bikes involves marketing and distributing electric bicycles to potential buyers, emphasizing features, benefits, and value propositions. This includes presenting through sales channels, addressing inquiries, negotiating pricing, and facilitating transactions to close sales.
+                </p>
+                <a href="#" class="btn">read more</a>
+            </div>
+            <div class="box">
+                <i class="fa fa-cogs"></i>
+                <h3>Accessories</h3>
+                <p>
+                    Accessories for e-bikes are additional components or add-ons that enhance functionality, comfort, and style. These include items such as racks, fenders, lights, locks, and helmets, which can be customized to suit individual preferences and riding needs.
+                </p>
+                <a href="#" class="btn">read more</a>
+            </div>
+            <div class="box">
+                <i class="fa fa-fax"></i>
+                <h3>Customer Service</h3>
+                <p>
+                    Customer service for e-bikes involves providing responsive support to users before, during, and after their purchase. This includes addressing inquiries, troubleshooting issues, handling returns or repairs, and ensuring customer satisfaction for a positive experience and brand loyalty.
+                </p>
+                <a href="#" class="btn">read more</a>
+            </div>
+            <div class="box">
+                <i class="fa fa-gavel"></i>
+                <h3>E-Bike Insurance</h3>
+                <p>
+                    E-bike insurance is coverage specifically designed to protect electric bicycles and their riders against theft, damage, liability, and other risks.
+                </p>
+                <a href="#" class="btn">read more</a>
+            </div>
+            <div class="box">
+                <i class="fa fa-cog"></i>
+                <h3>E-bike Repair</h3>
+                <p>
+                    Repair involves fixing or restoring a damaged or malfunctioning e-bike to ensure it functions properly and safely. This includes diagnosing issues, replacing or repairing components, and conducting maintenance to keep the e-bike in good working condition.
+                </p>
+                <a href="#" class="btn">read more</a>
+            </div>
+           </div>
+       </section>
+       <!--End Of services-->
+       <section class="featured">
+        <h1 class="heading">Featured <span>E-Bike</span></h1>
+        <div class="swiper-container featured-slider">
+            <div class="swiper-wrapper wrapper">
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/HERO2YELLOW.jpg" alt="">
+                    <h3>Latest Model: E-Bike Hero-2</h3>
+                   
+                    <div class="price"><span>Price : </span> &#8369;108,000</div>
+                    <a href="#" class="btn">Add to Cart</a>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ARSMOONWHITE.jpg" alt="">
+                    <h3>Latest model: E-Bike ARS</h3>
+                    
+                    <div class="price"><span>Price : </span> &#8369;34,000</div>
+                    <a href="#" class="btn">Add to Cart</a>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERVS3MAROON.jpg" alt="">
+                    <h3>Latest model: E-bike ERVS3</h3>
+                    
+                    <div class="price"><span>Price : </span> &#8369;59,800</div>
+                    <a href="#" class="btn">Add to Cart</a>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/TK10.jpg" alt="">
+                    <h3>Latest model: E-bike TK10</h3>
+                   
+                    <div class="price"><span>Price : </span> &#8369;38,800</div>
+                    <a href="#" class="btn">Add to Cart</a>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/EP2MAROON.jpg" alt="">
+                    <h3>Latest model: E-Bike EP-2</h3>
+                    
+                    <div class="price"><span>Price : </span> &#8369;38,800</div>
+                    <a href="#" class="btn">Add to Cart</a>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERVMINI_w5mb.jpg" alt="">
+                    <h3>Latest model: E-Bike ERV Mini</h3>
+                   
+                    <div class="price"><span>Price : </span> &#8369;37,800</div>
+                    <a href="#" class="btn">Add to Cart</a>
+                </div>
+            </div>
+            
+            <div class="swiper-container featured-slider secondSlider">
+                <div class="swiper-wrapper wrapper">
+                    <div class="swiper-slide box">
+                        <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/160507162655390_w6kk.jpg" alt="">
+                        <h3>Latest model: E-bike DCY</h3>
+                       
+                        <div class="price"><span>Price : </span> &#8369;132,000</div>
+                        <a href="#" class="btn">Add to Cart</a>
+                    </div>
+                    <div class="swiper-slide box">
+                        <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/160507162655390_awzl.jpg" alt="">
+                        <h3>Latest model: E-Bike EP</h3>
+                       
+                        <div class="price"><span>Price : </span> &#8369;31,800</div>
+                        <a href="#" class="btn">Add to Cart</a>
+                    </div>
+                    <div class="swiper-slide box">
+                        <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/WARRIOR_jhij.jpg" alt="">
+                        <h3>Latest model: E-Bike WARRIOR</h3>
+                       
+                        <div class="price"><span>Price : </span> &#8369;59,800</div>
+                        <a href="#" class="btn">Add to Cart</a>
+                    </div>
+                    <div class="swiper-slide box">
+                        <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERVS2GREY2_i6ou.jpg" alt="">
+                        <h3>Latest model: E-Bike ERVS2</h3>
+                        
+                        <div class="price"><span>Price : </span> &#8369;59,800</div>
+                        <a href="#" class="btn">Add to Cart</a>
+                    </div>
+                    <div class="swiper-slide box">
+                        <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/WSPWHT.jpg" alt="">
+                        <h3>Latest model: E-Bike WSP</h3>
+                       
+                        <div class="price"><span>Price : </span> &#8369;43,800</div>
+                        <a href="#" class="btn">Add to Cart</a>
+                    </div>
+                    <div class="swiper-slide box">
+                        <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/GB2_pxmu.jpg" alt="">
+                        <h3>Latest model: E-Bike GB2</h3>
+                        
+                        <div class="price"><span>Price : </span> &#8369;24,000</div>
+                        <a href="#" class="btn">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+       </section>
+       <!--End Of featured-->
+       <section class="top">
+        <h1 class="heading">Top-Rated <span>E-Bike</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERVS2GREY2_i6ou.jpg" alt="">
+                </div>
+                <div class="content">
+                    
+                    <h3>Best E-Bike MotorCycle</h3>
+                    <p>
+                        Has a MP3 Bluetooth and Speaker. Key start switch, Light and Wiper Combination switch including Double Flash Switch, Self-locking Steering wheel. 2 seats in front and 2 seats in back
+                    </p>
+                    <a href="#" class="btn">add to cart</a>
+                    <span class="price">PHP 88,000</span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/WSPWHT.jpg" alt="">
+                </div>
+                <div class="content">
+
+                    <h3>Best E-Bike MotorCycle</h3>
+                    <p>
+                        3 Level Shift Gear near accelerator, Extra compartment for your belongings. A USB Port, and available in lead-acid and lithium iron battery
+                    </p>
+                    <a href="#" class="btn">add to cart</a>
+                    <span class="price">PHP 43,800</span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="https://prof562e926-pic5.ysjianzhan.cn/upload/ERV2WHT.jpg" alt="">
+                </div>
+                <div class="content">
+                   
+                    <h3>Best E-bike MotorCycle</h3>
+                    <p>
+                        ERV2 is unit that are child friendly and perfect for driving your children to school.
+                    </p>
+                    <a href="Cart.html" class="btn">add to cart</a>
+                    <span class="price">PHP 42,800</span>
+                </div>
+            </div>
+        </div>
+       </section>
+       <!--End Of top-->
+      
+    </section>
+    </body>
+</html>
+<script src="swiper/js/swiper.min.js"></script>
+<script>
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop:true,
+      centeredSlides:true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  </script>
+  </html>
